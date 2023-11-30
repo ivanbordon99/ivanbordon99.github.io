68b921bbfeedaad8968524979ed0e466ec44e21e
Iván Bordón <IvanBordon99@gmail.com>
2023-11-29 23:44:44 -0300

    script.js

--- calculadora/script.js
+++ calculadora/script.js
@@ -2,12 +2,6 @@ const CALCULAR = document.getElementById('calcular');
 const ERROR = document.getElementById('error');
 const FLU = document.getElementById('flu');
 const MAN = document.getElementById('man');
-function calcular(entrada){
-
-
-  
-}
-
 CALCULAR.addEventListener('click', () => {
     const DATO = document.getElementById('peso').value;
     console.log('dato ingresado: ', DATO);
