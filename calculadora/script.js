83adc55295b0d76f8165b5e02bc7285adcc43393
Iván Bordón <IvanBordon99@gmail.com>
2023-11-30 09:12:22 -0300

    script.js

--- calculadora/script.js
+++ calculadora/script.js
@@ -3,6 +3,23 @@ const ERROR = document.getElementById('error');
 const FLU = document.getElementById('flu');
 const MAN = document.getElementById('man');
 
+function calcFlujo(peso){
+    let resto = peso;
+    let flujo = 0;
+    if (resto>20){
+        let aux = resto-20;
+        flujo += aux*1;
+        resto -= aux;
+    } 
+    if (resto>10){
+        let aux = resto-10;
+        flujo += aux*2;
+        resto -= aux;
+    }
+    flujo += resto*4;
+    return flujo;
+}
+
 CALCULAR.addEventListener('click', () => {
     const DATO = document.getElementById('peso').value
     //validamos que se cargue un dato:
@@ -20,3 +37,4 @@ CALCULAR.addEventListener('click', () => {
         MAN.style.display = 'none';
     }
 })
+
