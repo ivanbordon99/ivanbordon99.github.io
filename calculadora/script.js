15ceefd8b3b22be94fa01ecf02b561c0bdafaf9a
Iván Bordón <IvanBordon99@gmail.com>
2023-11-29 23:37:58 -0300

    script.js

--- calculadora/script.js
+++ calculadora/script.js
@@ -2,3 +2,12 @@ const CALCULAR = document.getElementById('calcular');
 const ERROR = document.getElementById('error');
 const FLU = document.getElementById('flu');
 const MAN = document.getElementById('man');
+function calcular(entrada){
+
+
+  
+}
+
+CALCULAR.addEventListener('click', () => {
+    console.log('Se hizo click!')
+})
